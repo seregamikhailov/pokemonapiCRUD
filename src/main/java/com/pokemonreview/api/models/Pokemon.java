2cb714b0bdffef6da56fd22548df5147d4ffbbde
Sergey Mikhailov <seregamikhailov05@mail.ru>
2024-04-03 00:40:05 +0300

    Create two primitive  models

---
+++ src/main/java/com/pokemonreview/api/models/Pokemon.java
@@ -0,0 +1,11 @@
+package com.pokemonreview.api.models;
+
+import lombok.Data;
+
+@Data
+public class Pokemon {
+    private int id;
+    private String name;
+    private String type;
+
+}
---
+++ src/main/java/com/pokemonreview/api/models/Review.java
@@ -0,0 +1,12 @@
+package com.pokemonreview.api.models;
+
+import lombok.Data;
+
+@Data
+public class Review {
+    private int id;
+    private String title;
+    private String content;
+    private int start;
+
+}
