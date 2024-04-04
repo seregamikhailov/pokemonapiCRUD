acee289d82f5532edd13d71c05a2d8f2b275ce11
Sergey Mikhailov <seregamikhailov05@mail.ru>
2024-04-04 11:06:37 +0300

    add getAll to Service

--- src/main/java/com/pokemonreview/api/controllers/PokemonController.java
+++ src/main/java/com/pokemonreview/api/controllers/PokemonController.java
@@ -25,12 +25,8 @@ public class PokemonController {
 
 
     @GetMapping("pokemon")
-    public ResponseEntity<List<Pokemon>> getPokemons() {
-        List<Pokemon> pokemons = new ArrayList<>();
-        pokemons.add(new Pokemon(1, "Squirtle", "Water"));
-        pokemons.add(new Pokemon(2, "Pikachu", "Electric"));
-        pokemons.add(new Pokemon(3, "Charmander", "Fire"));
-        return ResponseEntity.ok(pokemons);
+    public ResponseEntity<List<PokemonDto>> getPokemons() {
+        return new ResponseEntity<>(pokemonService.getAllPokemon(),HttpStatus.OK);
     }
     @GetMapping("pokemon/{id}")
     public Pokemon pokemonDetail(@PathVariable int id) {
