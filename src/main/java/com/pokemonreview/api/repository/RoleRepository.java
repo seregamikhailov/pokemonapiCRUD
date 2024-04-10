d7fce8bdb5275695fdd680d3ce57a6dbc98c972a
Sergey Mikhailov <seregamikhailov05@mail.ru>
2024-04-10 23:28:30 +0300

    fix RoleRepository

--- src/main/java/com/pokemonreview/api/repository/RoleRepository.java
+++ src/main/java/com/pokemonreview/api/repository/RoleRepository.java
@@ -1,6 +1,6 @@
 package com.pokemonreview.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import javax.management.relation.Role;
+import com.pokemonreview.api.models.Role;
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role,Integer> {
